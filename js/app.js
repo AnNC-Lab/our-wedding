eae3bd83c4c18568125ff49e57c6bf978e1548e7
AnNguyen <anuet59@gmail.com>
2025-01-08 22:49:59 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import { guest } from './guest/guest.js';
+import { comment } from './comment/comment.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.undangan = guest.init();
+    window.comment = comment;
+});
