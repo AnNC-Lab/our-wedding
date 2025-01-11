8a8ba7d8e78d30fa6d3a8bc2a32b543e08f8063c
AnNguyen <anuet59@gmail.com>
2025-01-11 20:16:09 +0700

    fix code

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import { guest } from './guest/guest.js';
 import { comment } from './comment/comment.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.undangan = guest.init();
+    window.ancLab = guest.init();
     window.comment = comment;
 });
 
